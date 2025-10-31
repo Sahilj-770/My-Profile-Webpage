339b02ed913c4b867c0134dbfcf794ba0cbde12d
Sahil Jadhav <jadhavsahil0027@gmail.com>
2025-10-31 17:15:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function welcomeMSG(){
+    alert("hello champ's, welcome to ITSEA workshop");
+}
+
+function Darkmode(){
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+}
+
